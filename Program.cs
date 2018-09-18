74ea313ba039d0073874c7f38f56b78e6e10617e
Aren Villanueva <arenvillanueva@yomogi-soft.com>
2018-09-18 21:40:30 +1000

    Handle turning the robot around, spin me right round yeah!

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace toy_robot
             var r = new Robot();
             r.Place(t, 1, 0, "north");
             r.Move(t);
+            r.Left();
+            r.Left();
+            r.Move(t);
+            r.Report();
+            r.Right();
+            r.Move(t);
             r.Report();
         }
     }
--- Robot.cs
+++ Robot.cs
@@ -5,11 +5,18 @@ namespace toy_robot
 {
     public sealed class Robot
     {
-        private static List<string> availableDirections = new List<string> {
+        private static readonly List<string> availableDirections = new List<string> {
             "north",
             "east",
             "south",
             "west"
+        };
+
+        private static readonly Dictionary<string, string> reverseDirLookup = new Dictionary<string, string> {
+            {"01", "north"},
+            {"10", "east"},
+            {"0-1", "south"},
+            {"-10", "west"}
         };
         // Initialise position as null to ensure we have
         // an indicator for when the robot hasn't been placed
@@ -69,10 +76,42 @@ namespace toy_robot
             }
         }
 
+        private void UpdateDirection()
+        {
+            var key = string.Format("{0}{1}", vx, vy);
+            currentDirection = reverseDirLookup[key];
+        }
+
+        public void Left()
+        {
+            if (x != null && y != null) {
+                // Rotate x, y 90 degrees left is simply -y, x
+                var tx = vx;
+                vx = (short)-vy;
+                vy = tx;
+
+                UpdateDirection();
+            }
+        }
+
+        public void Right()
+        {
+            if (x != null && y != null) {
+                // Rotate x, y 90 degrees right is simply y, -x
+                var tx = vx;
+                vx = vy;
+                vy = (short)-tx;
+
+                UpdateDirection();
+            }
+        }
+
         public void Report()
         {
             if (x != null && y != null) {
-                Console.WriteLine(string.Format("{0},{1}", x, y));
+                Console.WriteLine(
+                    string.Format("{0},{1},{2}", x, y, currentDirection.ToUpper())
+                );
             }
         }
     }
