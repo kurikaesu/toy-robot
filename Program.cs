f5729c5444a861f833845df0b0fc70d1f0766f1f
Aren Villanueva <arenvillanueva@yomogi-soft.com>
2018-09-18 21:59:08 +1000

    Added ability to read from a supplied input file. Added 1 test file

--- Program.cs
+++ Program.cs
@@ -6,17 +6,54 @@ namespace toy_robot
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0) {
+                Console.WriteLine("Please specify an instruction file");
+                return;
+            }
+
+            var filename = args[0];
+
             var t = new Table();
             var r = new Robot();
-            r.Place(t, 1, 0, "north");
-            r.Move(t);
-            r.Left();
-            r.Left();
-            r.Move(t);
-            r.Report();
-            r.Right();
-            r.Move(t);
-            r.Report();
+
+            using (var file = new System.IO.StreamReader(filename)) {
+                string line = null;
+                while ((line = file.ReadLine()) != null) {
+                    var parts = line.Split(" ");
+                    if (parts.Length == 1) {
+                        switch (parts[0].ToLower()) {
+                            case "move":
+                                r.Move(t);
+                                break;
+                            case "left":
+                                r.Left();
+                                break;
+                            case "right":
+                                r.Right();
+                                break;
+                            case "report":
+                                r.Report();
+                                break;
+                            default:
+                                // Don't know this command
+                                break;
+                        }
+                    } else if (parts.Length == 2 && parts[0].ToLower() == "place") {
+                        var pArgs = parts[1].Split(",");
+                        try {
+                            var x = Convert.ToUInt16(pArgs[0]);
+                            var y = Convert.ToUInt16(pArgs[1]);
+                            var direction = pArgs[2];
+                            r.Place(t, x, y, direction);
+                        } catch (Exception e) {
+                            Console.WriteLine(
+                                string.Format("Encountered input error: {0}", e.Message)
+                            );
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
